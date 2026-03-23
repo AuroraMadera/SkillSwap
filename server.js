59f3dc07c8ad168c84d5f3b6899da864c411f447
Aurora <auroramadera94@gmail.com>
2026-03-22 22:00:09 -0600

    cambios en el js para aparición de pagina registrar y modificación en la página regisrar

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/pages/index.html');
-});
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
 });
 
 app.get('/registrar', (req, res) => {
-  res.sendFile(__dirname + '/public/pages/registrar.html');
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'registrar.html'));
 });
 
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
